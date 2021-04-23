68c76e9f25cb19cca6c9ea5010a4079cb52e0b8f
MikhailPimenov <misha4git@yandex.ru>
2021-04-24 01:13:09 +0300

    works fine. Type annotation needed. Refactor needed: create-update functions for user_poll need to be refactored, perhaps use template-method-pattern

--- polls/settings.py
+++ polls/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&h1ryb&5!8r8n_^k!)w0a(_-yjo7=vi_suo80-#yrlitc%b-7='
+SECRET_KEY = '10z6fklqc-%urue-+vk3dr!(k*^n7++(mpee*@k-vxay7(t366'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'poll',
+    'rest_framework',
+    'drf_yasg2',
+    'nested_admin',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- polls/urls.py
+++ polls/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
+    path('nested_admin/', include('nested_admin.urls')),
     path('admin/', admin.site.urls),
+    path('', include('poll.urls')),
 ]
+
+urlpatterns += doc_urls
