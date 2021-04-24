7e6d8c6ada041c7aeb51a07555c3ceefb3ba2abb
MikhailPimenov <misha4git@yandex.ru>
2021-04-24 22:03:51 +0300

    template-method-pattern replaced by template function with function pointers sent to it

--- poll/views.py
+++ poll/views.py
@@ -9,8 +9,6 @@ from .swagger import user_poll_put_parameters_for_documentation, \
     poll_get_parameters_for_documentation, user_poll_delete_parameters_for_documentation, \
     user_poll_get_parameters_for_documentation
 
-from .create_update_user_poll import AbstractClass, CreateUserPoll, UpdateUserPoll
-
 
 class PollAPIView(GenericAPIView):
     serializer_class = PollSerializer
@@ -91,30 +89,72 @@ class UserPollsAPIView(GenericAPIView):
         user_id = int(request.query_params["user_id"])
         return self.get_user_polls_with_user_id(user_id)
 
-    # def create_new_user_poll(self, request, user_id, poll_id):
-    #     user_poll = self.get_body_and_add_parameters(request, user_id, poll_id)
-    #
-    #     serializer = UserPollSerializer(data=user_poll)
-    #
-    #     question_ids = self.get_question_ids_and_send_to_serializer(user_poll, serializer)
-    #     SUCCESS_STATUS_CONST = 201
-    #     return self.modify_database_and_send_response(question_ids, user_id, poll_id, serializer, SUCCESS_STATUS_CONST,
-    #                                                   'created')
-    #
-    # def update_existing_user_poll(self, request, user_id, poll_id):
-    #     user_poll = self.get_body_and_add_parameters(request, user_id, poll_id)
-    #
-    #     user_poll_saved = UserPoll.objects.get(user_id=user_id, poll=poll_id)
-    #     serializer = UserPollSerializer(instance=user_poll_saved, data=user_poll, partial=True)
-    #
-    #     question_ids = self.get_question_ids_and_send_to_serializer(user_poll, serializer)
-    #     SUCCESS_STATUS_CONST = 200
-    #     return self.modify_database_and_send_response(question_ids, user_id, poll_id, serializer, SUCCESS_STATUS_CONST,
-    #                                                   'updated')
+    @staticmethod
+    def get_serializer_for_creating(user_poll):
+        serializer = UserPollSerializer(data=user_poll)
+        return serializer
 
     @staticmethod
-    def create_or_update(abstract_class: AbstractClass, request, user_id, poll_id):
-        return abstract_class.template_method(request, user_id, poll_id)
+    def get_serializer_for_updating(user_poll):
+        user_poll_saved = UserPoll.objects.get(
+            user_id=user_poll["user_id"],
+            poll=user_poll["poll"]
+        )
+        serializer = UserPollSerializer(
+            instance=user_poll_saved,
+            data=user_poll,
+            partial=True
+        )
+        return serializer
+
+    @staticmethod
+    def get_response_data_for_creating():
+        SUCCESS_STATUS_CONST = 201
+        SUCCESS_ACTION_MESSAGE_CONST = "created"
+        return SUCCESS_STATUS_CONST, SUCCESS_ACTION_MESSAGE_CONST
+
+    @staticmethod
+    def get_response_data_for_updating():
+        SUCCESS_STATUS_CONST = 200
+        SUCCESS_ACTION_MESSAGE_CONST = "updated"
+        return SUCCESS_STATUS_CONST, SUCCESS_ACTION_MESSAGE_CONST
+
+    @staticmethod
+    def are_question_ids_valid(question_ids: list, poll_id) -> bool:
+        for question_id in question_ids:
+            if not Question.objects.filter(pk=question_id).exists() or \
+                    not Question.objects.get(pk=question_id).poll.id == int(poll_id):
+                return False
+        return True
+
+    def template_for_create_and_update(self, request, get_serializer, get_data_for_response):
+        user_poll = request.data
+        user_id = request.query_params["user_id"]
+        poll_id = request.query_params["poll_id"]
+        user_poll["user_id"] = user_id
+        user_poll["poll"] = poll_id
+
+        serializer = get_serializer(user_poll)
+
+        question_ids = []
+        for user_answer in user_poll.get("user_answers"):
+            question_ids.append(user_answer["question_id"])
+        serializer.set_question_ids(question_ids)
+
+        if not self.are_question_ids_valid(question_ids, poll_id):
+            return Response({"error": f'poll={poll_id} does not have questions you listed'}, status=400)
+
+        if not serializer.is_valid(raise_exception=True):
+            return Response({"error": serializer.errors}, status=400)
+
+        serializer.save()
+
+        SUCCESS_STATUS_CONST, SUCCESS_ACTION_MESSAGE_CONST = get_data_for_response()
+
+        return Response(
+            {"success": f'user_poll user_id={user_id} poll={poll_id} {SUCCESS_ACTION_MESSAGE_CONST} successfully'},
+            status=SUCCESS_STATUS_CONST
+        )
 
     @swagger_auto_schema(**user_poll_put_parameters_for_documentation.get_parameters())
     def put(self, request):
@@ -123,18 +163,24 @@ class UserPollsAPIView(GenericAPIView):
             return Response({"error": "request parameters do not contain 'user_id' or/and 'poll_id'"},
                             status=400)
 
-        user_id = request.query_params["user_id"]
-        poll_id = request.query_params["poll_id"]
-
-        if not Poll.objects.filter(id=poll_id).exists():
-            return Response({"error": f'poll with id={poll_id} does not exist'}, status=404)
+        if not Poll.objects.filter(id=request.query_params["poll_id"]).exists():
+            return Response({"error": f'poll with id={request.query_params["poll_id"]} does not exist'},
+                            status=404)
 
         try:
-            return self.create_or_update(UpdateUserPoll(), request, user_id, poll_id)
+            return self.template_for_create_and_update(
+                request=request,
+                get_serializer=self.get_serializer_for_updating,
+                get_data_for_response=self.get_response_data_for_updating,
+            )
         except ValueError:
             return Response({"error": "invalid request parameters"}, status=400)
         except ObjectDoesNotExist:
-            return self.create_or_update(CreateUserPoll(), request, user_id, poll_id)
+            return self.template_for_create_and_update(
+                request=request,
+                get_serializer=self.get_serializer_for_creating,
+                get_data_for_response=self.get_response_data_for_creating,
+            )
 
     @staticmethod
     @swagger_auto_schema(**user_poll_delete_parameters_for_documentation.get_parameters())
